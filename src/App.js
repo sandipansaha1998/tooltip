616acebcd7d4b6ddfaeab59c0cc41833efc93bbf
Sandipan Saha <sandipansaha1998@icloud.com>
2023-06-29 12:04:46 +0530

    Add Tooltip

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 function App() {
+  const [showToolTip, setShowToolTip] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="App" style={{}}>
+      <h4 style={{ marginBottom: `50px` }}>Tooltip Example</h4>
+      <h4 style={{ position: `relative` }}>
+        {" "}
+        <span
+          style={{
+            border: `2px solid black`,
+            padding: `10px`,
+          }}
+          onMouseEnter={() => {
+            setShowToolTip(true);
+          }}
+          onMouseLeave={() => {
+            setShowToolTip(false);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          {" "}
+          Hover me
+        </span>
+        {showToolTip && (
+          <span
+            style={{
+              position: "absolute",
+              top: `-36px`,
+              backgroundColor: `black`,
+              color: "white",
+              padding: `3px`,
+            }}
+          >
+            Info
+          </span>
+        )}
+      </h4>
     </div>
   );
 }
